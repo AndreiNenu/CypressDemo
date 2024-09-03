e8aefd8084d3f89644d1cfdf6de9190fd09c6a78
AndreiNenu <andreinenu1794@gmail.com>
2024-09-03 14:04:09 +0300

    added register file and changes to config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,15 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
     },
+
+    baseUrl: "https://parabank.parasoft.com/parabank/",
+
+    env: {
+      logLevel: "VERBOSE"
+    },
+
+    watchForFileChanges: false
   },
 });
---
+++ cypress/e2e/register.cy.ts
@@ -0,0 +1,19 @@
+before('', () => {
+})
+
+beforeEach('', () => {
+    cy.visit('register.htm')
+})
+
+after('', () => {
+})
+
+describe('Register tests', () => {
+
+it('Check register user with valid data', () => {
+
+
+    
+})
+
+})
